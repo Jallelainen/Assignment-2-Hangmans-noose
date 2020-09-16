a9c6fb8d020ac333082b4fea5df73d0089c26ccb
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-16 13:32:30 +0200

    Add project files.

---
+++ Assignment-2-Hangmans-noose/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment_2_Hangmans_noose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
