3f05426fd1086b4caf8d6671c636b8fa6f0b3d48
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-18 11:03:28 +0200

    cleaned up code

--- Assignment-2-Hangmans-noose/Program.cs
+++ Assignment-2-Hangmans-noose/Program.cs
@@ -6,49 +6,41 @@ namespace Assignment_2_Hangmans_noose
 {
     class Program
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
-             string MenuItems = "-------Hangman-------\nNew Game (1)\nExit(-1)\nYour choice: ";
-             string NotValidInput = "That is not a valid input. Please try again.";
-             string AnyKey = "Hit any key to continue.";
-             bool keepLooping = true;
-             while (keepLooping)
-             {
-                
+            string MenuItems = "-------Hangman-------\nNew Game (1)\nExit(-1)\nYour choice: ";
+            string NotValidInput = "That is not a valid input. Please try again.";
+            string AnyKey = "Hit any key to continue.";
+            bool keepLooping = true;
+            while (keepLooping)
+            {
 
-                     WriteLine(MenuItems);
-                     var choice = int.Parse(Console.ReadLine() ?? "");
-                     switch (choice)
-                     {
-                         case 1:
-                             StartGame();
-                             break;
-                         case -1:
-                             keepLooping = false;
-                             break;
-                         default:
-                             Console.ForegroundColor = ConsoleColor.Red;
-                             WriteLine(NotValidInput);
-                             break;
-                     }
-                     Console.ResetColor();
-                     WriteLine(AnyKey);
-                     Console.ReadKey();
-                     Console.Clear();
-                 
-                 /*catch
-                 {
-                     Console.ForegroundColor = ConsoleColor.Red;
-                     WriteLine(NotValidInput);
-                     Console.ResetColor();
-                     WriteLine(AnyKey);
-                     Console.ReadKey();
-                     Console.Clear();
-                 }*/
-             }
-         
 
-            
+                WriteLine(MenuItems);
+                var choice = int.Parse(Console.ReadLine() ?? "");
+                switch (choice)
+                {
+                    case 1:
+                        StartGame();
+                        break;
+                    case -1:
+                        keepLooping = false;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        WriteLine(NotValidInput);
+                        break;
+                }
+                Console.ResetColor();
+                WriteLine(AnyKey);
+                Console.ReadKey();
+                Console.Clear();
+
+
+            }
+
+
+
         }
         public static string WordGenerator()
         {
@@ -63,7 +55,7 @@ namespace Assignment_2_Hangmans_noose
             wordArray[7] = "revoke";
             wordArray[8] = "intention";
             wordArray[9] = "patient";
-            
+
             Random random = new Random();
             var randomNumber = random.Next(0, 9);
             string mysteryWord = wordArray[randomNumber];
@@ -77,70 +69,107 @@ namespace Assignment_2_Hangmans_noose
             string NotValidInput = "That is not a valid input. Please try again.";
             StringBuilder wrongLetters = new StringBuilder();
             string theWord = WordGenerator();
-            StringBuilder mysteryWord = (HideWord(theWord));
+            // StringBuilder mysteryWord = (HideWord(theWord));
             char[] rightGuess = new char[theWord.Length];
+            InitUnderscore(rightGuess);
 
-            
+
             char playerGuess;
-            for (int i = 9; i > 0; i--)//Lives loop
+
+
+            for (int i = 10; i > 0; i--)//Lives loop
             {
-                
-               // Console.Clear();
-                Console.WriteLine("You have {0} lives left", i + 1);
+
+                Console.Clear();
+                Console.WriteLine("You have {0} lives left", i);
                 Console.WriteLine("Your guesses:" + wrongLetters);
-                Console.WriteLine(mysteryWord);
+                PrintRightGuesses(rightGuess);
                 WriteLine(WhatType);
 
-                string[] wordLength = new string[mysteryWord.Length];
+                string[] wordLength = new string[theWord.Length];
 
                 string playerGuess1 = Console.ReadLine();
-                Console.WriteLine("sdfdsfdsdfdsf" + playerGuess1.Length);
-                if(playerGuess1.Length == 1)
+                if (playerGuess1.Length == 1)
                 {
-                playerGuess = char.Parse(playerGuess1);
-                // playerGuess = char.Parse(Console.ReadLine());
-                
-                string stringPlayerGuess = playerGuess.ToString();
-                for (int j = 0; j < theWord.Length; j++)
-                {
-                
-                    if (playerGuess == theWord[j])
+                    if (wrongLetters.ToString().Contains(playerGuess1) || rightGuess.ToString().Contains(playerGuess1))
                     {
-                        wordLength[j] = stringPlayerGuess;
-                        rightGuess[j] = playerGuess;
+                        i++;
+                        Console.WriteLine("you already guessed that");
                     }
-                    else if (playerGuess.ToString() == theWord)
+                    else if (theWord.Contains(playerGuess1))
                     {
-                        Console.WriteLine("you won biatch");
+                        playerGuess = char.Parse(playerGuess1);
+                        for (int j = 0; j < theWord.Length; j++)
+                        {
+
+                            if (playerGuess == theWord[j])
+                            {
+                                rightGuess[j] = playerGuess;
+                            }
+
+
+                        }
+
+                    }
+                    else
+                    {
+                        wrongLetters.Append(playerGuess1);
+
                     }
 
-                }
-                wrongLetters.Append(stringPlayerGuess);
-               
+
 
                 }
                 else if (playerGuess1.Length > 1)
                 {
-                    Console.WriteLine("whaaat");
+                    if (playerGuess1 == theWord)
+                    {
+                        Console.WriteLine("you won\nHit any key: ");
+                        break;
+
+                    }
                 }
+                Console.WriteLine("Hit any key to continue");
+                Console.ReadKey(true);
             }
-            Console.WriteLine("you lost suckah");
+            Console.WriteLine("you lost");
+
+
+
         }
 
-        static StringBuilder HideWord(string mysteryWord)
+        static void InitUnderscore(char[] hiddenWord)
         {
-            
+            for (int i = 0; i < hiddenWord.Length; i++)
+            {
+                hiddenWord[i] = '_';
+            }
+        }
+
+        static void PrintRightGuesses(char[] rightGuess)
+        {
+            for (int i = 0; i < rightGuess.Length; i++)
+            {
+                Console.Write(rightGuess[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+       /* static StringBuilder HideWord(string mysteryWord)
+        {
+
             StringBuilder hiddenWord = new StringBuilder();
 
-            for (var i = 0; i <mysteryWord.Length; i++) 
+            for (var i = 0; i < mysteryWord.Length; i++)
             {
                 hiddenWord.Append("_ ");
             }
 
             return hiddenWord;
-      
 
-        }
+
+        }*/
 
         public static void WriteLine(string text)
         {
