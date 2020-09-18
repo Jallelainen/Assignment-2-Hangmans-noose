93390fba3d7d256b1565edb2a4835853ea34178c
marcus.jarlevid@gmail.com <marcus.jarlevid@gmail.com>
2020-09-18 13:55:40 +0200

    Added an if to end the game

--- Assignment-2-Hangmans-noose/Program.cs
+++ Assignment-2-Hangmans-noose/Program.cs
@@ -44,7 +44,7 @@ namespace Assignment_2_Hangmans_noose
         }
         public static string WordGenerator()
         {
-            string[] wordArray = new string[10];
+            string[] wordArray = new string[10]; //declares new array
             wordArray[0] = "bark";
             wordArray[1] = "potery";
             wordArray[2] = "cooperation";
@@ -56,9 +56,9 @@ namespace Assignment_2_Hangmans_noose
             wordArray[8] = "intention";
             wordArray[9] = "patient";
 
-            Random random = new Random();
-            var randomNumber = random.Next(0, 9);
-            string mysteryWord = wordArray[randomNumber];
+            Random random = new Random(); //declares random variable
+            var randomNumber = random.Next(0, 9); // assigns an int number from array to a variable
+            string mysteryWord = wordArray[randomNumber]; // creates variable with string assigned from array via the random int number
 
             return mysteryWord;
 
@@ -66,79 +66,89 @@ namespace Assignment_2_Hangmans_noose
         static void StartGame()
         {
             string WhatType = "\nWhat would you like to guess? ";
-            string NotValidInput = "That is not a valid input. Please try again.";
-            StringBuilder wrongLetters = new StringBuilder();
-            string theWord = WordGenerator();
-            // StringBuilder mysteryWord = (HideWord(theWord));
-            char[] rightGuess = new char[theWord.Length];
-            InitUnderscore(rightGuess);
-
-
+            StringBuilder wrongLetters = new StringBuilder(" "); //creates a stringbuilder intended for wrong letter guesses.
+            string theWord = WordGenerator(); //assigns the word by calling WordGenerator method.
+            char[] rightGuess = new char[theWord.Length]; //creates new array for the right guesses. Makes it as long as the word.
+            InitUnderscore(rightGuess); // takes the array above and calls method that returns the word but in underscores.
             char playerGuess;
 
 
-            for (int i = 10; i > 0; i--)//Lives loop
+            for (int i = 10; i >= 0; i--)//Lives loop. Will loop 10 times
             {
 
                 Console.Clear();
-                Console.WriteLine("You have {0} lives left", i);
-                Console.WriteLine("Your guesses:" + wrongLetters);
-                PrintRightGuesses(rightGuess);
+                Console.WriteLine("You have {0} lives left", i); //string message that calls{0} which iteration the for-loop is in.
+                Console.WriteLine("Your guesses: " + wrongLetters); //displays previous wrong guesses.
+                PrintRightGuesses(rightGuess); // calls method to display the hidden word. Will insert right guesses in right place.
                 WriteLine(WhatType);
 
-                string[] wordLength = new string[theWord.Length];
-
-                string playerGuess1 = Console.ReadLine();
-                if (playerGuess1.Length == 1)
+                //string[] wordLength = new string[theWord.Length]; //creates new string array the length of the word.
+                if (i > 0)
                 {
-                    if (wrongLetters.ToString().Contains(playerGuess1) || rightGuess.ToString().Contains(playerGuess1))
-                    {
-                        i++;
-                        Console.WriteLine("you already guessed that");
-                    }
-                    else if (theWord.Contains(playerGuess1))
-                    {
-                        playerGuess = char.Parse(playerGuess1);
-                        for (int j = 0; j < theWord.Length; j++)
-                        {
 
-                            if (playerGuess == theWord[j])
+                    string playerGuess1 = Console.ReadLine(); // user inputs guess, gets assigned to variable.
+
+                    if (playerGuess1.Length == 1) //if the user guess amount of letters equals 1.
+                    {
+                        if (wrongLetters.ToString().Contains(playerGuess1) || Array.IndexOf(rightGuess, playerGuess1) >= 0) //if user guess already exists in the wrong or right guesses.(compares to stringbuilder and array)
+                        {
+                            i++; // adds a iteration (life) to the for-loop.
+                            Console.WriteLine("you already guessed that");
+                        }
+                        else if (theWord.Contains(playerGuess1)) //if the word contains the users guess.
+                        {
+                            playerGuess = char.Parse(playerGuess1); //turns user guess from string to char.
+                            for (int j = 0; j < theWord.Length; j++) // goes through as many iterations as the word is long.
                             {
-                                rightGuess[j] = playerGuess;
+
+                                if (playerGuess == theWord[j]) // if user guess matches with position in the word[j].
+                                {
+                                    rightGuess[j] = playerGuess; // inserts user guess in rightWord array in correct position.
+                                }
+
+
                             }
 
+                        }
+                        else
+                        {
+                            wrongLetters.Append(playerGuess1);// wrong guess gets put in stringbuilder.
 
                         }
 
+
+
                     }
-                    else
+                    else if (playerGuess1.Length > 1) // if user guess is bigger than 1 letter.
                     {
-                        wrongLetters.Append(playerGuess1);
+                        if (playerGuess1 == theWord) // if user guess equals to the word
+                        {
+                            Console.Clear();
+                            Console.WriteLine("-------| | CONGRATULATIONS! | |------- \nWell done, you've won!!! ");
+                            Console.WriteLine("You had {0} lives left", i); //{0} calls iteration from loop
+                            Console.WriteLine("You guessed: " + wrongLetters);
+                            Console.WriteLine("The right word was " + theWord);
+                            break;
 
+                        }
                     }
-
-
 
                 }
-                else if (playerGuess1.Length > 1)
+                else if (i == 0)
                 {
-                    if (playerGuess1 == theWord)
-                    {
-                        Console.WriteLine("you won\nHit any key: ");
-                        break;
-
-                    }
+                    Console.Clear();
+                    Console.WriteLine("|-|-|THE GAME|-|-|\nYou just lost it...");
+                    break;
                 }
                 Console.WriteLine("Hit any key to continue");
                 Console.ReadKey(true);
             }
-            Console.WriteLine("you lost");
 
 
 
         }
 
-        static void InitUnderscore(char[] hiddenWord)
+        static void InitUnderscore(char[] hiddenWord) //
         {
             for (int i = 0; i < hiddenWord.Length; i++)
             {
@@ -155,21 +165,6 @@ namespace Assignment_2_Hangmans_noose
             Console.WriteLine();
         }
 
-
-       /* static StringBuilder HideWord(string mysteryWord)
-        {
-
-            StringBuilder hiddenWord = new StringBuilder();
-
-            for (var i = 0; i < mysteryWord.Length; i++)
-            {
-                hiddenWord.Append("_ ");
-            }
-
-            return hiddenWord;
-
-
-        }*/
 
         public static void WriteLine(string text)
         {
